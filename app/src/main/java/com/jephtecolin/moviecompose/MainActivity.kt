20d39f55b37a5760f2f29c5ac040515c9575526a
Jephte Colin <drakecolinj@gmail.com>
2022-12-28 12:47:50 -0400

    WIP splash

--- app/src/main/java/com/jephtecolin/moviecompose/MainActivity.kt
+++ app/src/main/java/com/jephtecolin/moviecompose/MainActivity.kt
@@ -10,11 +10,15 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.jephtecolin.moviecompose.ui.theme.MovieComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
+        Thread.sleep(2000L)
+
         setContent {
             MovieComposeTheme {
                 // A surface container using the 'background' color from the theme
