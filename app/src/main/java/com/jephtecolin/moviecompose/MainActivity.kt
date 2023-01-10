71e28a76cf32d63d70152a3b365fab2ae8b17e73
Jephte Colin <drakecolinj@gmail.com>
2023-01-10 12:11:06 -0400

    Add network connectivity check

--- app/src/main/java/com/jephtecolin/moviecompose/MainActivity.kt
+++ app/src/main/java/com/jephtecolin/moviecompose/MainActivity.kt
@@ -3,28 +3,43 @@ package com.jephtecolin.moviecompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Snackbar
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.graphics.Color
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.jephtecolin.moviecompose.data.ConnectivityObserver
+import com.jephtecolin.moviecompose.data.NetworkConnectivityObserver
 import com.jephtecolin.moviecompose.ui.theme.MovieComposeTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private lateinit var connectivityObserver: ConnectivityObserver
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
         Thread.sleep(2000L)
-
+        connectivityObserver = NetworkConnectivityObserver(applicationContext)
         setContent {
             MovieComposeTheme {
+                val status by connectivityObserver.observe().collectAsState(
+                    initial = ConnectivityObserver.Status.Unavailable
+                )
                 // A surface container using the 'background' color from the theme
-                MovieComposeApp()
+                Column() {
+                    if (status == ConnectivityObserver.Status.Unavailable ||
+                                status == ConnectivityObserver.Status.Lost) {
+                        Snackbar( action = { }, containerColor = Color(0xFF7C0A0A)) { Text(text = " There's no Internet")}
+                    }
+
+                    MovieComposeApp()
+                }
+
+
             }
         }
     }
---
+++ app/src/main/java/com/jephtecolin/moviecompose/data/ConnectivityObserver.kt
@@ -0,0 +1,12 @@
+package com.jephtecolin.moviecompose.data
+
+import kotlinx.coroutines.flow.Flow
+
+interface ConnectivityObserver {
+
+    fun observe(): Flow<Status>
+
+    enum class Status {
+        Available, Unavailable, Losing, Lost
+    }
+}
